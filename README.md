06f06a7b6d0b18c5895e9c3f10158a5c3e802651
kevin-meng <meng_fr595@163.com>
2023-05-10 23:34:25 +0800

    merge

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 use chatgpt to create a mentor help raise critique thought skill
 
+## 背景
+在AIGC大爆发的时代，个人将面临铺垫盖地的信息资料，其中不乏大量的虚假信息。如何不被迷惑，保持独立思考能力，使我们需要思考和解决的问题。
+
+**我们的解决方案**
+利用 chatGPT 来启发学生多维度看待问题，培养其批判性思维，提高问商。
+
+## 项目成员:
+[kevin-meng](https://github.com/kevin-meng)
 
 
-## Setup
+[JACOB152](https://github.com/JACOB152)
+
+
+## 界面截图
+![](./pic/screenshot.png)
+
+## 使用方法
 
 1. Install dependencies:
 
@@ -32,6 +46,7 @@ Once the script is started, you can go to the URL [http://localhost:8501](http:/
 ## License
 
 This project is released under the [MIT License](LICENSE).
+
 
 
   
--- chat.py
+++ chat.py
@@ -6,13 +6,30 @@ import os
 import openai
 from langchain.document_loaders.csv_loader import CSVLoader
 import re
+import pdfminer
+from pdfminer.high_level import extract_pages,extract_text
+import streamlit as st
 
 
 
-st.set_page_config(page_title="助思者", page_icon="💗", layout="wide",
+
+
+def load_pdf_data(file):
+    texts = ""
+    for page_layout in extract_pages(file):
+        for element in page_layout:
+            texts += element.get_text()
+            # st.write(element.get_text())
+    return texts
+
+
+
+
+st.set_page_config(page_title="", page_icon="💗", layout="wide",
 )
 
-
+st.image("./pic/mentor.png",width=200)
+# st.title("助思者")
 # Load environment variables from a .env file (containing OPENAI_API_KEY)
 load_dotenv()
 # Set the title for the Streamlit app
@@ -30,6 +47,7 @@ if api_base !='':
 
 
 
+
 def generate_response(prompt):
     # Generate a response using OpenAI's ChatCompletion API and the specified prompt
     completion = openai.ChatCompletion.create(
@@ -43,21 +61,43 @@ def generate_response(prompt):
 
 
 
-def get_text():
+def get_text(text=''):
     # Create a Streamlit input field and return the user's input
-    input_text = st.text_area('输入分析文本', height=5).strip()
+    input_text = st.text_area('输入分析文本', text,height=2).strip()
     return input_text
 
 
-col1, col2 = st.columns(2)
+
+
+base_prompt = """
+        你是一个具有极强的批判性思维的导师，你总能从正反（好或坏）两个方面去看待问题和现象。并会利用问题温和的引导对方进行自我思考。
+        你将按照json的格式进行返回回答。
+        {"反面":["1. ...", "2. ....", "3. ..."],
+        "反面":["1. ...", "2. ....", "3. ..."],
+        "结论":"...."}
+        ----------------
+        """
+
+
+col1, col2,col3 = st.columns([3, 2,1])
 
 with col1:
-    user_input = get_text()
+    st.info("培养批判性思维，提高问商")
+    expander01 = st.expander("上传文件")
+    # TODO 上传pdf 识别
+    uploaded_file = expander01.file_uploader("", type="pdf", accept_multiple_files=False,  label_visibility="hidden")
+    if uploaded_file is not None:
+        text = load_pdf_data(uploaded_file)
+        user_input = get_text(text)
+    else:
+        user_input = get_text()
+
+    user_input = base_prompt + user_input
     # st.text(user_input)
     click = st.button('发送')
 
 with col2:
-    # TODO 上传pdf 识别
+
     pass
 
 
@@ -73,12 +113,12 @@ if click:
             col3, col4 = st.columns(2)
 
             with col3:
-                st.markdown("#### 正面：")
+                st.markdown("#### :innocent: 正面：")
                 for line in text['正面']:
                     st.markdown("- " + line)
 
             with col4:
-                st.markdown("#### 反面：")
+                st.markdown("#### :imp: 反面：")
                 for line in text['反面']:
                     st.markdown("- " + line)
 
@@ -90,3 +130,5 @@ if click:
 
     else:
         st.text("请输入文本")
+
+
